bf45e7affa38b4c02a5b5e17b08374e9d1cb655a
Justin Lamb <justinlamb@me.com>
2022-03-17 13:26:56 -0700

    disallow invalid locations

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,20 +3,17 @@ module WINCONDITION
         winning_configs = [[2, 4, 6], [10, 12, 14], [18, 20, 22], # Rows
                         [2, 10, 18], [4, 12, 20], [6, 14, 22], # Columns
                         [2, 12, 22], [6, 12, 18]] # Diags
-        winner = ''
         winning_configs.each do |config|
             config_checker = []
             config.each do |location|
                 config_checker << @board[location]
             end
             if config_checker.uniq == ['X']
-                winner = 'X'
+                puts "Winner is X"
                 return true
-                return winner
             elsif config_checker.uniq == ['O']
-                winner = 'O'
+                puts "winner is O"
                 return true
-                return winner
             else
                 return false
             end
@@ -34,16 +31,22 @@ class Board
     end
 
     def mark(player, move_location)
-        puts self.board
-
-        self.board[move_location] = player
-        puts self.board
+        puts self.coords
+        # Check if location is already in use
+        invalid_selections = ["|", "\n", "X", "O"]
+        if invalid_selections.include?(self.board[move_location])
+            puts "Invalid selection. Choose an empty location: "
+            move_location = gets.strip.to_i
+            mark(player, move_location)
+        else
+            self.board[move_location] = player
+            puts self.board
+        end
     end
 
     include WINCONDITION
 
 end
-
 
 
 def game
@@ -55,17 +58,15 @@ def game
 
     game_board = Board.new()
 
-    while !game_board.is_winner?
+    while !game_board.is_winner? do
         puts "#{player_x}, where will you move?"
         x_move = gets.strip
         game_board.mark('X', x_move.to_i)
+
         puts "#{player_o}, where will you move?"
         o_move = gets.strip
         game_board.mark('O', o_move.to_i)
     end
-    puts "Winner is: #{winner}"
 end
-
-
 
 game()
